2de226aebf2c5c950b1ae7059e2aa39cce764459
Anastasia Anastasia <anastasiaanastasia@MacBook-Air-Anastasia.local>
2023-04-27 19:32:33 +0400

    My first commit

---
+++ configuration.py
@@ -0,0 +1,6 @@
+URL_SERVICE = "https://66661200-2ef6-454e-8eff-d861121f3287.serverhub.praktikum-services.ru"
+DOC_PATH = "/docs/"
+LOG_MAIN_PATH = "/api/logs/main/"
+USERS_TABLE_PATH = "/api/db/resources/user_model.csv"
+CREATE_USER_PATH = "/api/v1/users/"
+PRODUCTS_KITS_PATH = "/api/v1/products/kits/"
